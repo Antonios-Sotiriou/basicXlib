50d74650141c7558ebc4b54db3e328be26dc4274
Antonios Sotiriou <antonios.sotiriou@hotmail.gr>
2022-11-22 11:39:29 +0100

    Basic X11 - xlib applications Code Base.

---
+++ Makefile
@@ -0,0 +1,23 @@
+# Compile all c files to crwindow executable!
+
+CC = gcc
+CFLAGS = -Wall -Werror -g #-O2 -Os -Og -O0
+OBJ = basic
+RM = rm
+LINKS = -lX11 -lm
+
+install:
+	./install.sh;
+
+uninstall:
+	sudo apt remove libx11-dev;
+
+all:
+	$(CC) $(CFLAGS) main.c locale.c -o $(OBJ) $(LINKS);
+
+exec:
+	./anvil
+
+clean:
+	sudo apt autoremove -y;
+
---
+++ main.c
@@ -0,0 +1,215 @@
+/* general headers */
+#include <stdio.h>
+#include <stdlib.h>
+#include <X11/Xlib.h>
+#include <math.h>
+#include <unistd.h>
+
+enum { Win_Close, Win_Name, Atom_Type, Atom_Last};
+
+#define WIDTH                     800
+#define HEIGHT                    800
+#define XWorldToScreen            ( (1 + c.t[i].v[j].x) * (wa.width / 2.00) )
+#define YWorldToScreen            ( (1 + c.t[i].v[j].y) * (wa.height / 2.00) )
+
+#define POINTERMASKS              ( ButtonPressMask )
+#define KEYBOARDMASKS             ( KeyPressMask )
+#define EXPOSEMASKS               ( StructureNotifyMask | ExposureMask )
+
+/* Some Global Variables */
+Display *displ;
+Window win;
+Pixmap pixmap;
+XWindowAttributes wa;
+XSetWindowAttributes sa;
+GC gc;
+XGCValues gcvalues;
+Atom wmatom[Atom_Last];
+
+static int MAPCOUNT = 0;
+static int RUNNING = 1;
+
+/* Event handling functions. */
+const static void clientmessage(XEvent *event);
+const static void reparentnotify(XEvent *event);
+const static void mapnotify(XEvent *event);
+const static void expose(XEvent *event);
+const static void resizerequest(XEvent *event);
+const static void configurenotify(XEvent *event);
+const static void buttonpress(XEvent *event);
+const static void keypress(XEvent *event);
+
+/* Xlib relative functions and event dispatcher. */
+static KeySym get_keysym(XEvent *event);
+const static void pixmapupdate(void);
+const static void pixmapdisplay(void);
+const static void atomsinit(void);
+const static int board(void);
+static void (*handler[LASTEvent]) (XEvent *event) = {
+    [ClientMessage] = clientmessage,
+    [ReparentNotify] = reparentnotify,
+    [MapNotify] = mapnotify,
+    [Expose] = expose,
+    [ResizeRequest] = resizerequest,
+    [ConfigureNotify] = configurenotify,
+    [ButtonPress] = buttonpress,
+    [KeyPress] = keypress,
+};
+
+/* Project specific inludes. */
+#include "header_files/locale.h"
+
+/* Testing */
+
+const static void clientmessage(XEvent *event) {
+
+    if (event->xclient.data.l[0] == wmatom[Win_Close]) {
+        printf("WM_DELETE_WINDOW");
+        
+        XFree(gc);
+        XFreePixmap(displ, pixmap);
+        XDestroyWindow(displ, win);
+        XCloseDisplay(displ);
+        
+        RUNNING = 0;
+    }
+}
+const static void reparentnotify(XEvent *event) {
+
+    printf("reparentnotify event received\n");
+    XGetWindowAttributes(displ, win, &wa);
+}
+const static void mapnotify(XEvent *event) {
+
+    printf("mapnotify event received\n");
+
+    if (MAPCOUNT) {
+        pixmapdisplay();
+    } else {
+        MAPCOUNT = 1;
+    }
+}
+const static void expose(XEvent *event) {
+
+    printf("expose event received\n");
+    pixmapupdate();
+}
+const static void resizerequest(XEvent *event) {
+
+    printf("resizerequest event received\n");
+}
+const static void configurenotify(XEvent *event) {
+
+    if (!event->xconfigure.send_event) {
+        printf("configurenotify event received\n");
+        XGetWindowAttributes(displ, win, &wa);
+    }
+}
+const static void buttonpress(XEvent *event) {
+
+    printf("buttonpress event received\n");
+    printf("Mouse coordinates X: %d -- Y: %d\n", event->xbutton.x, event->xbutton.y);
+}
+const static void keypress(XEvent *event) {
+
+    KeySym keysym = get_keysym(event);
+    // printf("Key Pressed: %ld\n", keysym);
+    // printf("\x1b[H\x1b[J");
+    switch (keysym) {
+
+        default :
+            return;
+    }
+
+    pixmapdisplay();
+}
+static KeySym get_keysym(XEvent *event) {
+
+    /* Get user text input */
+    XIM xim;
+    XIC xic;
+    char *failed_arg;
+    XIMStyles *styles;
+
+    xim = XOpenIM(displ, NULL, NULL, NULL);
+    if (xim == NULL) {
+        perror("keypress() - XOpenIM()");
+    }
+    failed_arg = XGetIMValues(xim, XNQueryInputStyle, &styles, NULL);
+    if (failed_arg != NULL) {
+        perror("keypress() - XGetIMValues()");
+    }
+    xic = XCreateIC(xim, XNInputStyle, XIMPreeditNothing | XIMStatusNothing, XNClientWindow, win, NULL);
+    if (xic == NULL) {
+        perror("keypress() - XreateIC()");
+    }
+    XSetICFocus(xic);
+
+    KeySym keysym = 0;
+    char buffer[32];
+    Status status = 0;
+    Xutf8LookupString(xic, &event->xkey, buffer, 32, &keysym, &status);
+    if (status == XBufferOverflow) {
+        perror("Buffer Overflow...\n");
+    }
+    return keysym;
+}
+const static void pixmapupdate(void) {
+    pixmap = XCreatePixmap(displ, win, wa.width, wa.height, wa.depth);
+    XCopyArea(displ, win, pixmap, gc, 0, 0, wa.width, wa.height, 0, 0);
+}
+const static void pixmapdisplay(void) {
+    XCopyArea(displ, pixmap, win, gc, 0, 0, wa.width, wa.height, 0, 0);
+}
+const static void atomsinit(void) {
+
+    wmatom[Win_Close] = XInternAtom(displ, "WM_DELETE_WINDOW", False);
+    XSetWMProtocols(displ, win, &wmatom[Win_Close], 1);
+
+    wmatom[Win_Name] = XInternAtom(displ, "WM_NAME", False);
+    wmatom[Atom_Type] =  XInternAtom(displ, "STRING", False);
+    XChangeProperty(displ, win, wmatom[Win_Name], wmatom[Atom_Type], 8, PropModeReplace, (unsigned char*)"Anvil", 5);
+}
+/* General initialization and event handling. */
+const static int board(void) {
+
+    XEvent event;
+
+    displ = XOpenDisplay(NULL);
+    if (displ == NULL) {
+        fprintf(stderr, "Warning: Board - XOpenDisplay()\n");
+        return EXIT_FAILURE;
+    }
+    int screen = XDefaultScreen(displ);
+
+    /*  Root main Window */
+    sa.event_mask = EXPOSEMASKS | KEYBOARDMASKS | POINTERMASKS;
+    sa.background_pixel = 0x000000;
+    win = XCreateWindow(displ, XRootWindow(displ, screen), 0, 0, WIDTH, HEIGHT, 0, CopyFromParent, InputOutput, CopyFromParent, CWBackPixel | CWEventMask, &sa);
+    XMapWindow(displ, win);
+
+    gcvalues.graphics_exposures = False;
+    gc = XCreateGC(displ, win, GCForeground | GCGraphicsExposures, &gcvalues);
+
+    atomsinit();
+
+    while (RUNNING) {
+
+        XNextEvent(displ, &event);
+        
+        if (handler[event.type])
+            handler[event.type](&event);
+    }
+    return EXIT_SUCCESS;
+}
+int main(int argc, char *argv[]) {
+
+    if (locale_init())
+        fprintf(stderr, "Warning: Main -locale()\n");
+
+    if (board())
+        return EXIT_FAILURE;
+    
+    return EXIT_SUCCESS;
+}
+
